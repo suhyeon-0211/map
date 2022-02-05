38aace45ad1818e973b21303b02bb008f2a4c4a0
DESKTOP-3C35ERR\kitew <kitewinpsh@naver.com>
2022-02-05 15:56:42 +0900

    aws upload 세팅

--- src/main/java/com/map/MapApplication.java
+++ src/main/java/com/map/MapApplication.java
@@ -2,9 +2,10 @@ package com.map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MapApplication {
+public class MapApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MapApplication.class, args);
